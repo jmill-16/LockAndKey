b6bf8c02409db3aeb29db5026c8306effbe77654
jmill-16 <jasonsroom16@gmail.com>
2024-03-26 06:18:34 -0400

    Main Menu, Credits, and Pause Menu
    pause menu buttons and slider are non functional for some reason, it just won't let me click them. Pause menu at least comes up though when escape is pressed. Credits buttons and main menu buttons work fine though.

---
+++ Lock_And_Key/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    private string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Credits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    public void returnToMM() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        GameHandler_PauseMenu.GameisPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
