78d96a514da51777d7759308282e05ab52104002
connorg2404 <connortownson@gmail.com>
2024-04-02 17:43:52 -0400

    gravity powerup and tutorial improvements
    Fixed the gravity powerup so that the gravity inverts. In addition, I added some extra platforms and jumps to the end of the tutorial.

--- Lock_And_Key/Assets/Scripts/GravityPowerup.cs
+++ Lock_And_Key/Assets/Scripts/GravityPowerup.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class GravityPowerup : MonoBehaviour
 {
+    private SpriteRenderer playerSprite;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
         // Debug.Log("Some Collision detected");
         if (collision.gameObject.tag == "Player") {
             Debug.Log("Player Collision detected");
-            collision.attachedRigidbody.gravityScale = 2;
-            StartCoroutine(gravityEffect(collision));
+            Physics2D.gravity = new Vector2(0, 9.8f);
+            playerSprite = collision.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+            playerSprite.flipY = true;
+            StartCoroutine(gravityEffect(collision, playerSprite));
         }
     }
 
-    IEnumerator gravityEffect(Collider2D collision) {
-          yield return new WaitForSeconds(10f);
-          collision.attachedRigidbody.gravityScale = 1;
+    IEnumerator gravityEffect(Collider2D collision, SpriteRenderer playerSprite) {
+          yield return new WaitForSeconds(8f);
+          playerSprite.flipY = false;
+          Physics2D.gravity = new Vector2(0, -9.8f);
      }
 }
