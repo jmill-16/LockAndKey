e32339533b7750771ce787587c8158bf43da0d5f
yvette-m <yvette.roos@tufts.edu>
2024-04-09 00:15:26 -0400

    flip(character and skeleton)
    they both face the right direction now / will turn to the direction they are walking. Both have sound effects but should be re-recorded.

--- Lock_And_Key/Assets/Scripts/EnemyPatrol.cs
+++ Lock_And_Key/Assets/Scripts/EnemyPatrol.cs
@@ -9,17 +9,25 @@ public class EnemyPatrol : MonoBehaviour
     private Rigidbody2D rb;
     private Transform currentPoint;
     public float speed;
+    private bool FaceLeft = true; // determine which way player is facing.
+    AudioSource audioSourse;
+    private Vector3 hMove;
+
+    // private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         currentPoint = pointB.transform;
+        audioSourse = GetComponent<AudioSource>();
+        // anim.SetBool("isRunning", true);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector2 point = currentPoint.position - transform.position;
         if(currentPoint == pointB.transform)
         {
@@ -28,16 +36,34 @@ public class EnemyPatrol : MonoBehaviour
         else
         {
             rb.velocity = new Vector2(-speed, 0);
+            audioSourse.Play();
         }
 
         if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
         {
             currentPoint = pointA.transform;
+            if(!FaceLeft) {
+                flip();
+            }
         }
         if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
         {
             currentPoint = pointB.transform;
+            if(FaceLeft) {
+                flip();
+            }
         }
-        
+    }
+
+    private void flip()
+    {
+        if(!audioSourse.isPlaying)
+        {
+                audioSourse.Play();
+        }
+        FaceLeft = !FaceLeft;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
