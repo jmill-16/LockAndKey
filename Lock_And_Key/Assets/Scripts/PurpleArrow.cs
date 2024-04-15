8f75027791d6a664da3eba030385ad8b92c4b3e4
ifranc03 <isabel.francis@tufts.edu>
2024-04-15 01:32:50 -0400

    level3part2

--- Lock_And_Key/Assets/Scripts/PurpleArrow.cs
+++ Lock_And_Key/Assets/Scripts/PurpleArrow.cs
@@ -26,20 +26,20 @@ public class PurpleArrow : MonoBehaviour
         if (!gameHandler.selectedHiddenPower) {
             Debug.Log("color");
             if (gameHandler.viewPurpleOn) {
-                Debug.Log("pruple");
+                //Debug.Log("pruple");
                 foreach (GameObject purplearrow in allPurpleArrows) 
                     purplearrow.SetActive(true); 
                 foreach (GameObject bluearrow in allBlueArrows) 
                     bluearrow.SetActive(false); 
             } else {
-                Debug.Log("blue");
+                //Debug.Log("blue");
                 foreach (GameObject purplearrow in allPurpleArrows) 
                     purplearrow.SetActive(false); 
                 foreach (GameObject bluearrow in allBlueArrows) 
                     bluearrow.SetActive(true); 
             }
         } else {
-            Debug.Log("disappear");
+            //Debug.Log("disappear");
             foreach (GameObject purplearrow in allPurpleArrows) 
                 purplearrow.SetActive(false); 
             foreach (GameObject bluearrow in allBlueArrows) 
