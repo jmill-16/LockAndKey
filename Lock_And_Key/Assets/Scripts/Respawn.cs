e23a92d61cccb858762b1ab4f70f5e5bd1c95bf9
jmill-16 <jasonsroom16@gmail.com>
2024-04-16 03:56:41 -0400

    level 2 done, fading platforms now reappear when respawning

--- Lock_And_Key/Assets/Scripts/Respawn.cs
+++ Lock_And_Key/Assets/Scripts/Respawn.cs
@@ -7,11 +7,21 @@ public class Respawn : MonoBehaviour
     public float threshold;
     private Vector3 spawn = new Vector3(0,0,0);
 
+    GameObject[] fadingPlats;
+
+    Color startColor;
+
+    Color fullAlpha;
+
     // Start is called before the first frame update
     void Start()
     {
         spawn = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
         threshold = -10;
+        fadingPlats = GameObject.FindGameObjectsWithTag("FadingPlatform");
+        Debug.Log("num of fading plats = " + fadingPlats.Length);
+        startColor = fadingPlats[0].transform.GetChild(0).GetComponent<SpriteRenderer>().material.color;
+        fullAlpha = new Color(startColor.r, startColor.g, startColor.b, 1f);
     }
 
     // Update is called once per frame
@@ -19,6 +29,10 @@ public class Respawn : MonoBehaviour
     {
         if (transform.position.y < threshold) {
             transform.position = spawn;
+            foreach (GameObject plat in fadingPlats) {
+                plat.SetActive(true);
+                plat.transform.GetChild(0).GetComponent<SpriteRenderer>().material.color = fullAlpha;
+            }
         }
     }
 }
--- Lock_And_Key/Assets/Scripts/respawnSpikes.cs
+++ Lock_And_Key/Assets/Scripts/respawnSpikes.cs
@@ -7,11 +7,20 @@ public class RespawnSpikes : MonoBehaviour
     private Vector3 spawn = new Vector3(0,0,0);
     public GameObject player;
 
+    GameObject[] fadingPlats;
+
+    Color startColor;
+
+    Color fullAlpha;
     // Start is called before the first frame update
     void Start()
     {
         spawn = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
         Debug.Log(spawn);
+        fadingPlats = GameObject.FindGameObjectsWithTag("FadingPlatform");
+        Debug.Log("num of fading plats = " + fadingPlats.Length);
+        startColor = fadingPlats[0].transform.GetChild(0).GetComponent<SpriteRenderer>().material.color;
+        fullAlpha = new Color(startColor.r, startColor.g, startColor.b, 1f);
     }
 
     
@@ -19,6 +28,10 @@ public class RespawnSpikes : MonoBehaviour
         if (other.gameObject.tag == "Player"){
             Debug.Log("working");
             player.transform.position = spawn;
+            foreach (GameObject plat in fadingPlats) {
+                plat.SetActive(true);
+                plat.transform.GetChild(0).GetComponent<SpriteRenderer>().material.color = fullAlpha;
+            }
         }
     }
 }
