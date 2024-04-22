f0f1325e0092ce113635b0813cfc2ca3e6cc94ca
helena-f <43306951+helena-f@users.noreply.github.com>
2024-04-21 21:19:02 -0400

    bad dialog

---
+++ Lock_And_Key/Assets/Scripts/NPCController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+   public void Interact() {
+        Debug.Log("interact with NPC");
+   }
+}
